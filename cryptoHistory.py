28eb2fc4fd98961e6b326815e6598461af87594e
Diego <diealdon@gmail.com>
2021-11-01 11:04:55 +0000

    Limpieza de código, añadido CSV con datos desde 2013

--- cryptoHistory.py
+++ cryptoHistory.py
@@ -4,10 +4,7 @@ import argparse
 import pandas as pd
 from datetime import datetime
 from IPython.display import display
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
-
+import csv  
 
 # Parseamos los valores de entrada, el intervalo de fechas que queremos analizar.
 parser = argparse.ArgumentParser()
@@ -50,19 +47,11 @@ data = pd.concat(historical_object)
 # Eliminamos las columnas que consideramos innecesarias
 data.drop(data.columns.difference(['Rank','Nombre', 'Símbolo', 'Cap. de Mercado', 'Precio']), 1, inplace=True)
 
-#eliminamos los indices
-#data.reset_index(drop=True, inplace=True)
-
 #Creamos un dataframe desde el dataset
 df= pd.DataFrame(data)
-df['Precio'] = df['Precio'].str.replace('$', '')
-df['Cap. de Mercado'] = df['Cap. de Mercado'].str.replace('$', '')
 
 print(df)
 print(f'Descarga completa')
-
-#df['Precio'].astype(str).astype(double)
-#print(df.dtypes)
 
 #importamos a csv
 df.to_csv('coins.csv', index = False)
