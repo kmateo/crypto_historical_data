2ed022324018f8323135a57e2dd03994175104f3
diealpa <73941471+diealpa@users.noreply.github.com>
2021-12-29 17:07:54 +0000

    Update tratamiento_netflix.py

--- tratamiento_netflix.py
+++ tratamiento_netflix.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import csv
 import numpy as np
+from datetime import datetime
 
 #Cargamos csv generado
 df_net = pd.read_csv("Netflix_titulos.csv")
 
+############################### LIMPIEZA ###############################
+#Borrado de columnas innecesarias
 df_net = df_net.drop(columns = ['description'])
+df_net = df_net.drop(columns = ['cast'])
 
- 
+#df_net['date_added'] = df_net['date_added'].apply(lambda x: pd.to_datetime(str(x), format='%m%d%Y'))
+
+
+
+
+
+
+
+
+
+
 
 print(df_net.head(5))
-
 print(df_net.dtypes)
